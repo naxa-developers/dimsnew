ede316de9b192bfad01060fe99c8884864bef2c3
khadkap <khadkap007@gmail.com>
2019-04-19 12:40:57 +0545

    site home

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -57,7 +57,7 @@ class Home extends Admin_Controller
 	    $this->data['exposure_data']=$this->general->get_tbl_data_result('id,category_name,category_photo,category_table,category_type,public_view,language',$tbl,array('category_type'=>'Exposure_Data','language'=>$language),'id');
 	    $this->data['baseline_data']=$this->general->get_tbl_data_result('id,category_name,category_photo,category_table,category_type,public_view,language',$tbl,array('category_type'=>'Baseline_Data','language'=>$language),'id');
 	    $cat_tbl_list=$this->general->get_tbl_data_result('id,category_name,category_photo,category_table,category_type,public_view,language',$tbl,array('language'=>$language),'id');
-	    $this->data['drrdata'] = $this->general->get_tbl_data_result('slug,description,image,name','drrcategory',array('language'=>$language));
+	    //$this->data['drrdata'] = $this->general->get_tbl_data_result('slug,description,image,name','drrcategory',array('language'=>$language));
 	    // echo "<pre>";
 	    // print_r($this->data['beready']);
 	    // die;	
